7e9896a1a0aeab6cc8d20483b96458bb5f2fc82f
Erik Maximilian Forsman <Erik.Maximilian.Forsman@nav.no>
2020-10-15 15:15:33 +0200

    Lagt til test for helsesjekk & restrukturerte de testene som var.

--- src/test/kotlin/no/nav/omsorgspenger/journalforing/FerdigstillJournalforingTest.kt
+++ src/test/kotlin/no/nav/omsorgspenger/journalforing/FerdigstillJournalforingTest.kt
@@ -1,64 +1,23 @@
 package no.nav.omsorgspenger.journalforing
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.github.tomakehurst.wiremock.WireMockServer
-import io.ktor.client.HttpClient
-import io.ktor.client.features.json.JacksonSerializer
-import io.ktor.client.features.json.JsonFeature
 import java.util.UUID
 import kotlin.test.assertEquals
-import no.nav.helse.dusseldorf.testsupport.wiremock.getNaisStsTokenUrl
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.k9.rapid.behov.Behov
 import no.nav.k9.rapid.behov.Behovssekvens
-import no.nav.omsorgspenger.JoarkClient
-import no.nav.omsorgspenger.config.ServiceUser
-import no.nav.omsorgspenger.StsRestClient
-import no.nav.omsorgspenger.testutils.TestApplicationEngineExtension
-import no.nav.omsorgspenger.testutils.wiremock.journalpostApiBaseUrl
-import org.junit.jupiter.api.BeforeAll
+import no.nav.omsorgspenger.ApplicationContext
+import no.nav.omsorgspenger.registerApplicationContext
+import no.nav.omsorgspenger.testutils.ApplicationContextExtension
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
 
-@ExtendWith(TestApplicationEngineExtension::class)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ExtendWith(ApplicationContextExtension::class)
 internal class FerdigstillJournalforingTest(
-        private val wireMockServer: WireMockServer) {
+        private val applicationContext: ApplicationContext) {
 
-    internal val objectMapper: ObjectMapper = jacksonObjectMapper()
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .registerModule(JavaTimeModule())
-
-    val httpClient = HttpClient {
-        install(JsonFeature) { serializer = JacksonSerializer(objectMapper) }
-    }
-
-    private val client = JoarkClient(
-            env = mapOf(
-                    "JOARK_BASE_URL" to wireMockServer.journalpostApiBaseUrl(),
-                    "JOARK_API_GW_KEY" to "testApiKey"),
-            httpClient = httpClient,
-            stsRestClient = StsRestClient(
-                    env = mapOf(
-                            "STS_URL" to wireMockServer.getNaisStsTokenUrl(),
-                            "STS_API_GW_KEY" to "testApiKey"),
-                    serviceUser = ServiceUser("foo", "bar"),
-                    httpClient = httpClient
-            )
-    )
-    private var rapid = TestRapid()
-    private val journalforingMediator = JournalforingMediator(client)
-
-    @BeforeAll
-    internal fun setup() {
-        rapid.apply {
-            FerdigstillJournalforing(this, journalforingMediator)
-        }
+    private val rapid = TestRapid().apply {
+        this.registerApplicationContext(applicationContext)
     }
 
     @BeforeEach
--- src/test/kotlin/no/nav/omsorgspenger/testutils/wiremock/JournalpostApiMock.kt
+++ src/test/kotlin/no/nav/omsorgspenger/testutils/wiremock/JournalpostApiMock.kt
@@ -9,7 +9,6 @@ import com.github.tomakehurst.wiremock.matching.AnythingPattern
 
 private const val journalpostApiBasePath = "/journalpostapi-mock"
 private const val journalpostApiMockPath = "/rest/journalpostapi/v1/journalpost"
-
 private fun WireMockServer.stubOppdaterJournalpost(): WireMockServer {
     WireMock.stubFor(
             WireMock.put(WireMock
@@ -17,7 +16,7 @@ private fun WireMockServer.stubOppdaterJournalpost(): WireMockServer {
                     .withHeader("Authorization", containing("Bearer"))
                     .withHeader("Content-Type", equalTo("application/json"))
                     .withHeader("Nav-Consumer-Token", AnythingPattern())
-                    .withHeader("x-nav-apiKey", AnythingPattern())
+                    .withHeader("x-nav-apiKey", equalTo("testApiKeyJoark"))
                     .withRequestBody(matchingJsonPath("$.journalpostId"))
                     .willReturn(
                     WireMock.aResponse()
@@ -46,5 +45,14 @@ private fun WireMockServer.stubFerdigstillJournalpost(): WireMockServer {
 }
 
 
-internal fun WireMockServer.stubJournalpostApi() = stubOppdaterJournalpost().stubFerdigstillJournalpost()
+internal fun WireMockServer.stubIsReady() : WireMockServer {
+    stubFor(WireMock.get("$journalpostApiBasePath/isReady")
+        .willReturn(WireMock.aResponse()
+            .withStatus(200)
+        ))
+    return this
+}
+
+
+internal fun WireMockServer.stubJournalpostApi() = stubOppdaterJournalpost().stubFerdigstillJournalpost().stubIsReady()
 internal fun WireMockServer.journalpostApiBaseUrl() = baseUrl() + journalpostApiBasePath
