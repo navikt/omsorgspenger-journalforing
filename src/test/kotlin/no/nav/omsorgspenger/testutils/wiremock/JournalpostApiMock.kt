857e8405f287e712498db6051626f29378332244
Emil Albrektsson <emil.albrektsson@nav.no>
2020-10-28 13:45:08 +0100

    Testfix

--- src/test/kotlin/no/nav/omsorgspenger/testutils/wiremock/JournalpostApiMock.kt
+++ src/test/kotlin/no/nav/omsorgspenger/testutils/wiremock/JournalpostApiMock.kt
@@ -12,21 +12,22 @@ private const val journalpostApiMockPath = "/rest/journalpostapi/v1/journalpost"
 private fun WireMockServer.stubOppdaterJournalpost(): WireMockServer {
     WireMock.stubFor(
             WireMock.put(WireMock
-                .urlPathMatching(".*$journalpostApiMockPath.*"))
-                .withHeader("Authorization", containing("Bearer"))
-                .withHeader("Content-Type", equalTo("application/json"))
-                .withHeader("Nav-Consumer-Token", AnythingPattern())
-                .withHeader("x-nav-apiKey", equalTo("testApiKeyJoark"))
-                .withRequestBody(matchingJsonPath("$.journalpostId"))
-                .withRequestBody(matchingJsonPath("$.sak.sakstype", equalTo("FAGSAK")))
-                .withRequestBody(matchingJsonPath("$.sak.fagsaksystem", equalTo("OMSORGSPENGER")))
-                .withRequestBody(matchingJsonPath("$.tema", equalTo("OMS")))
-                .withRequestBody(matchingJsonPath("$.bruker.idType", equalTo("FNR")))
-                .withRequestBody(matchingJsonPath("$.bruker.id"))
-                .willReturn(
-                    WireMock.aResponse()
-                            .withStatus(200)
-            )
+                    .urlPathMatching(".*$journalpostApiMockPath.*"))
+                    .withHeader("Authorization", containing("Bearer"))
+                    .withHeader("Content-Type", equalTo("application/json"))
+                    .withHeader("Nav-Consumer-Id", AnythingPattern())
+                    .withHeader("Nav-Callid", AnythingPattern())
+                    .withHeader("x-nav-apiKey", equalTo("testApiKeyJoark"))
+                    .withRequestBody(matchingJsonPath("$.journalpostId"))
+                    .withRequestBody(matchingJsonPath("$.sak.sakstype", equalTo("FAGSAK")))
+                    .withRequestBody(matchingJsonPath("$.sak.fagsaksystem", equalTo("OMSORGSPENGER")))
+                    .withRequestBody(matchingJsonPath("$.tema", equalTo("OMS")))
+                    .withRequestBody(matchingJsonPath("$.bruker.idType", equalTo("FNR")))
+                    .withRequestBody(matchingJsonPath("$.bruker.id"))
+                    .willReturn(
+                            WireMock.aResponse()
+                                    .withStatus(200)
+                    )
     )
 
     return this
@@ -38,7 +39,8 @@ private fun WireMockServer.stubFerdigstillJournalpost(): WireMockServer {
                     .urlPathMatching(".*$journalpostApiMockPath.*"))
                     .withHeader("Authorization", containing("Bearer"))
                     .withHeader("Content-Type", equalTo("application/json"))
-                    .withHeader("Nav-Consumer-Token", AnythingPattern())
+                    .withHeader("Nav-Consumer-Id", AnythingPattern())
+                    .withHeader("Nav-Callid", AnythingPattern())
                     .withHeader("x-nav-apiKey", AnythingPattern())
                     .withRequestBody(matchingJsonPath("$.journalfoerendeEnhet", containing("9999")))
                     .willReturn(
@@ -50,11 +52,11 @@ private fun WireMockServer.stubFerdigstillJournalpost(): WireMockServer {
 }
 
 
-internal fun WireMockServer.stubIsReady() : WireMockServer {
+internal fun WireMockServer.stubIsReady(): WireMockServer {
     stubFor(WireMock.get("$journalpostApiBasePath/isReady")
-        .willReturn(WireMock.aResponse()
-            .withStatus(200)
-        ))
+            .willReturn(WireMock.aResponse()
+                    .withStatus(200)
+            ))
     return this
 }
 
