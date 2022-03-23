e5253516c837ef604e481c89a61fa63c3de32062
Emil Albrektsson <61695840+albrektsson@users.noreply.github.com>
2022-03-23 08:00:14 +0100

    Gradle wrapper & dependency bumps (#78)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,11 +2,11 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junitJupiterVersion = "5.8.2"
-val k9rapidVersion = "1.20220215091703-a5f112c"
-val dusseldorfKtorVersion = "3.1.6.7-f2a96e8"
-val ktorVersion = "1.6.7"
+val k9rapidVersion = "1.20220322130655-5752ec9"
+val dusseldorfKtorVersion = "3.1.6.8-248832c"
+val ktorVersion = "1.6.8"
 val jsonassertVersion = "1.5.0"
-val orgJsonVersion = "20211205"
+val orgJsonVersion = "20220320"
 val mockkVersion = "1.12.3"
 val openhtmltopdfVersion = "1.0.10"
 val verapdfVersion = "1.20.1"
@@ -98,7 +98,7 @@ tasks {
     }
 
     withType<Wrapper> {
-        gradleVersion = "7.4"
+        gradleVersion = "7.4.1"
     }
 
 }
