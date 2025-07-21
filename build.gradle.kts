168cd54403f91e23edb5acbcb17cdcc362001485
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2025-07-21 09:29:33 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ val verapdfVersion = "1.28.2"
 val mainClass = "no.nav.omsorgspenger.AppKt"
 
 plugins {
-    kotlin("jvm") version "2.2.0"
+    kotlin("jvm") version "2.1.21"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.sonarqube") version "6.2.0.5505"
     jacoco
